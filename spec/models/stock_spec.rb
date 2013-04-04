9b684e6c728f85bca94dea2397e0f3fa838d8fda
Jason Rylance <ecnalyr@gmail.com>
2013-04-04 10:23:33 -0500

    Changed record frequency of a vcr test.

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Stock , :vcr, record: :all do
+describe Stock , :vcr do
   context "#52_week_high" do
     it "should show 52 week high" do
       stock = FactoryGirl.create(:stock)
